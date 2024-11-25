d81c5a3fc540d3cded0bcd64e538662150ac5b51
Ayush Anshuman Supakar <supakarayush@gmail.com>
2024-11-25 10:21:17 +0530

    Built the group chat controller with get, post, update and delete methods and added the routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,4 +10,14 @@ router.post("/auth/login", AuthController.login)
 //Chat Group
 router.post("/chat-group", authmiddlewear, ChatGroupController.store);
 
+router.get("/chat-group/:id", authmiddlewear, ChatGroupController.show);
+
+router.get("/chat-group", authmiddlewear, ChatGroupController.index);
+
+router.put("/chat-group/:id", authmiddlewear, ChatGroupController.update);
+
+router.delete("/chat-group/:id", authmiddlewear, ChatGroupController.destroy);
+
+
+
 export default router
